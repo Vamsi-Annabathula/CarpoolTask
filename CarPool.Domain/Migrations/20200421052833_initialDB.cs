309f58eab8130a74897209cd9855c5ef8f18f421
Vamsi Annabathula <vamsyannabathula@gmail.com>
2020-04-21 05:31:53 +0000

    Updated Db

---
+++ CarPool.Domain/Migrations/20200421052833_initialDB.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarPool.Domain.Migrations
+{
+    public partial class initialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<long>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: true),
+                    IsRemoved = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    VIN = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    Capacity = table.Column<int>(nullable: false),
+                    IsRemoved = table.Column<bool>(nullable: false),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rides",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Boarding = table.Column<string>(nullable: true),
+                    Destination = table.Column<string>(nullable: true),
+                    Distance = table.Column<decimal>(nullable: false),
+                    FarePerKm = table.Column<decimal>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    ViaPoints = table.Column<string>(nullable: true),
+                    AvailableCapacity = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    VehicleId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rides", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rides_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rides_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PassengerRides",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Boarding = table.Column<string>(nullable: true),
+                    Destination = table.Column<string>(nullable: true),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    Distance = table.Column<decimal>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: false),
+                    BookingStatus = table.Column<int>(nullable: false),
+                    RequestedSeats = table.Column<int>(nullable: false),
+                    FarePerKM = table.Column<decimal>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    RideId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassengerRides", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PassengerRides_Rides_RideId",
+                        column: x => x.RideId,
+                        principalTable: "Rides",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PassengerRides_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    RideId = table.Column<string>(nullable: false),
+                    Stars = table.Column<int>(nullable: false),
+                    RideId1 = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.RideId);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Rides_RideId1",
+                        column: x => x.RideId1,
+                        principalTable: "Rides",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PassengerRides_RideId",
+                table: "PassengerRides",
+                column: "RideId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PassengerRides_UserId",
+                table: "PassengerRides",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_RideId1",
+                table: "Ratings",
+                column: "RideId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rides_UserId",
+                table: "Rides",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rides_VehicleId",
+                table: "Rides",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PhoneNumber",
+                table: "Users",
+                column: "PhoneNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_UserId",
+                table: "Vehicles",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassengerRides");
+
+            migrationBuilder.DropTable(
+                name: "Ratings");
+
+            migrationBuilder.DropTable(
+                name: "Rides");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
